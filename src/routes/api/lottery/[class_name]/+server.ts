359fb5d4a3eef5f530b31bb035baa51cf401fef8
Eric Passmore <eric.passmore@gmail.com>
2025-04-20 17:39:29 -0700

    fixed lottery api to work with class codes

--- src/routes/api/lottery/[class_name]/+server.ts
+++ src/routes/api/lottery/[class_name]/+server.ts
@@ -1,17 +1,16 @@
 import {
-    deletePerformerLottery,
-    getPerformerLottery,
-    updatePerformerLottery
+    deleteClassLottery,
+    getClassLottery,
+    updateClassLottery
 } from "$lib/server/db";
 import {json} from "@sveltejs/kit";
-import { type LotteryInterface, pafe_series } from '$lib/server/common';
 import {createLottery} from "$lib/server/lottery";
 import { auth_code } from '$env/static/private';
 import { isAuthorized } from '$lib/server/apiAuth';
 
-export async function GET({params, request}) {
+export async function GET({params}) {
     try {
-        const res = await getPerformerLottery(params.id)
+        const res = await getClassLottery(params.class_name)
         if (res.rowCount != 1) {
             return json({status: 'error', message: 'Not Found'}, {status: 404});
         }
@@ -38,20 +37,16 @@ export async function PUT({url, cookies, params, request}) {
     }
 
     try {
-        const {lottery, base34Lottery} = await request.json();
-        const ticket: LotteryInterface = {
-            lottery: lottery,
-            base34Lottery: base34Lottery
-        }
+        const {class_name, lottery} = await request.json();
 
-        if (!ticket.lottery || !ticket.base34Lottery) {
+        if (!class_name || !lottery) {
             return {status: 400, body: {message: 'Missing Field, Try Again'}}
         } else {
-            const results = await updatePerformerLottery(params.id, pafe_series(), ticket)
+            const results = await updateClassLottery(class_name, lottery)
             if (results.rowCount != null && results.rowCount > 0) {
-                return json( {id: params.id}, {status: 200, body: {message: 'Update successful'}, headers: access_control_headers});
+                return json( {id: params.class_name}, {status: 200, body: {message: 'Update successful'}});
             } else {
-                return json({id: params.id}, {status: 500, body: {message: 'Update failed'}});
+                return json({id: params.class_name}, {status: 500, body: {message: 'Update failed'}});
             }
         }
     } catch (error) {
@@ -60,24 +55,22 @@ export async function PUT({url, cookies, params, request}) {
 }
 
 export async function POST({params, request}) {
-    const access_control_headers =  {
-        'Access-Control-Allow-Origin': '*', // Allow all hosts
-        'Access-Control-Allow-Methods': 'POST', // Specify allowed methods
-    }
     try {
-        if (await createLottery(params.id)) {
-            return json({id: params.id}, {status: 200, body: {message: 'Update successful'}, headers: access_control_headers});
+        const {class_name, lottery} = await request.json();
+
+        if (await createLottery(class_name, lottery)) {
+            return json({id: params.class_name}, {status: 200, body: {message: 'Update successful'}});
         } else {
-            return json({id: params.id}, {status: 500, body: {message: 'Update failed'}});
+            return json({id: params.class_name}, {status: 500, body: {message: 'Update failed'}});
         }
     } catch (error) {
         return json({status: 'error', message: 'Failed to process the request'}, {status: 500});
     }
 }
 
-export async function DELETE({params, request}) {
+export async function DELETE({params}) {
     try {
-        const results = await deletePerformerLottery(params.id)
+        const results = await deleteClassLottery(params.class_name)
     } catch (error) {
         return json({status: 'error', message: 'Failed to process the request'}, {status: 500});
     }
